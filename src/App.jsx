d4148e2e6de6ace76f9c89c9733748308c234555
Nuize01-dev <nuwanweerasooriyabgn@gmail.com>
2026-07-15 13:49:54 +0530

    Config tailwind css.

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,11 @@ import React from 'react';
 
 const App = () => {
     return (
-        <div>
-          
-        </div>
+        <>
+            <h1 className="text-3xl font-bold underline">
+                Hello world!
+            </h1>
+        </>
     );
 };
 
